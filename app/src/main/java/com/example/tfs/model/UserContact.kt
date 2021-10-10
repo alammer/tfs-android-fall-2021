effb86e66d53da3b22b58336549c925b3e522cbe
alammer <coinup@mail.ru>
2021-10-10 11:41:55 +0300

    start commit/remove test dirs from git

--- app/src/main/java/com/example/tfs/model/UserContact.kt
+++
@@ -1,9 +0,0 @@
-package com.example.tfs.model
-
-import android.os.Parcelable
-import androidx.annotation.Keep
-import kotlinx.parcelize.Parcelize
-
-@Keep
-@Parcelize
-data class UserContact(val name: String?, val phoneNumber: String?) : Parcelable
