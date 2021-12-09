ff366eac2e3fe76a65b4854d1c2c2ac5792a5014
alammer <coinup@mail.ru>
2021-12-09 08:18:17 +0300

    minor - resize stream expand icon for better clickability

--- app/src/main/java/com/example/tfs/util/RxExt.kt
+++ app/src/main/java/com/example/tfs/util/RxExt.kt
@@ -8,11 +8,11 @@ fun <T> Observable<T>.retryWhenError(retryCount: Int, delayInSeconds: Long): Obs
         errors.zipWith(
             Observable.range(1, retryCount),
             { throwable: Throwable, count: Int -> Pair(throwable, count) })
-            .flatMap { count: Pair<Throwable, Int> ->
-                if (count.second < retryCount) {
+            .flatMap { (throwable, count): Pair<Throwable, Int> ->
+                if (count < retryCount) {
                     Observable.timer(delayInSeconds, TimeUnit.SECONDS)
                 } else {
-                    Observable.error(count.first)
+                    Observable.error(throwable)
                 }
             }
     }
